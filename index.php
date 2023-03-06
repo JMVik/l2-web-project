305c67c6f20d25497180afcc49b9ac2a4ac87e43
Jean-Matthieu Toussaint <jean-matthieu.toussaint@etu.unistra.fr>
2023-03-06 14:06:01 +0100

    Copie code index.hmtl dans index.php

--- index.php
+++ index.php
@@ -3,8 +3,59 @@
 <head>
     <meta charset="UTF-8">
     <title>Projet Web</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    
+    <header>
+        <nav>
+            <img src="assets/img/nav_img.png" alt="">
+            <ul>
+                <li>Accueil</li>
+                <li>Contact</li>
+                <li>A propos</li>
+            </ul>
+        </nav>
+        <img src="assets/img/head_img2.png" alt="">
+    </header>
+    <main>
+        <nav>
+            <h2>Futures présentations</h2>
+            <article>
+                <section>
+                    <img src="assets/img/test.jpg" alt="">
+                    <h3>Evenement 1</h3>
+                    <h4>14 février</h4>
+                </section>
+            </article>
+            <footer>
+                <button>
+                    <span>Voir plus</span>
+                </button>
+            </footer>
+        </nav>
+        <nav>
+            <h2>Articles</h2>
+            <article>
+                <header>
+                    <img src="" alt="">
+                </header>
+                <section>
+                    <h3></h3>
+                    <p></p>
+                </section>
+                <footer>
+                    <button>
+                        <span>Voir plus</span>
+                    </button>
+                </footer>
+            </article>
+        </nav>
+        <button>
+            <span>Voir plus</span>
+        </button>
+    </main>
+    <footer>
+        
+    </footer>
 </body>
 </html>
